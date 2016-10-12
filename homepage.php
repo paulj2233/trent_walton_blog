71378563c6ccc3984b82b61cb5b9f3401bd9226a
PAUL JOHNSON <paulj2850@gmail.com>
2016-10-12 18:35:33 -0500

    first commit

---
+++ homepage.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="pear.css">
+	<style type="text/css">a {text-decoration: none}</style>
+</head>
+<body>
+
+<div class="header">
+
+	<img src="logo_TrentWalton.png" class="logo">
+
+	<div class="page">
+		<a href= "..." class="page__articles">ARTICLES</a>
+		<a href= "..." class="page__notes">NOTES </a>
+		<a href= "..." class="page__info">INFO </a>
+		<a href= "..." class="page__search">SEARCH </a>
+	</div>
+
+</div>
+
+<div class= "main">
+
+	<h3 class="bio"> I'm a designer and web builder who works with two of my best friends at <a href="http://www.Paravel.com" class= "business">Paravel</a>
+	</h3>
+
+	<div class="links">
+		<a href="https://twitter.com/TrentWalton">Twitter /</a> 
+	 	<a href="https://dribble.com/TrentWalton">Dribble /</a>
+		<a href="https://codepen.io.com/TrentWalton">Codepen /</a> 
+	  	<a href="https://github.com/TrentWalton">GitHub /</a>
+	  	<a href="https://letterboxd.com/TrentWalton">Letterboxd /</a>
+	  	<a href="https://godaytrip.com/u/TrentWalton">DayTrip</a>
+	</div>
+
+	<div class="project">
+
+		<img src="codingProject.pnf" class="project__logo">
+
+		<div class="project__description">Near and dear to our hearts, <a href="..." class="project">DayTrip</a> is a Paravel project designed to make it easy to find something great to do and someplace great to go.  Austin/Texas area private beta out now.</div>
+	</div>
+
+	<div class="project">
+
+		<img src="codingProject.pnf" class="project__logo">
+
+		<div class="project__description">We recently published two new <a href="...">TMFO</a> posts.  One for <a href="...">Burt Reynolds</a>, and another one called <a href="...">Punch Dancing</a>.</div>
+	</div>
+
+	<div class="project">
+
+		<img src="codingProject.pnf" class="project__logo">
+
+		<div class="project__description">We recently updated <a href="...">paravelinc.com</a> with some new portfolio items, talks, podcasts, interviews, and a brand new home page graphic from <a href="...">Curtis Jinkins</a>.</div>
+	</div>
+
+	<div class="project">
+
+		<img src="codingProject.pnf" class="project__logo">
+
+		<div class="project__description"><a href="...">Device Agnostic</a> summarizes how I view and approach building for the web.  My perception has been shaped while working on responsive, component-based systems on a large-ish scale.</div>
+	</div>
+
+	<div class="project">
+
+		<img src="codingProject.pnf" class="project__logo">
+
+		<div class="project__description">I was thrilled that <a href="...">Typekit</a> and <a href="...">Dave Shea</a> asked me to contribute a them to <a href="...">CSS Zen Garden</a> to celebrate its recent responsive update.  Read about the process <a href="...">here</a> and <a href="...">here</a>.</div>
+	</div>
+
+	</div class="more">
+		<p> More:<a href="..." class="more__info">Info</a>,
+		<a href="..." class="more__info">Articles</a>, or
+		<a href="..." class="more__info">Notes</a>
+	</div>
+
+
+</div>
+
+<div class="footer">
+
+	<div class="bottom">
+		<a href= "..." > <img src="..." class="bottom__twitter">FOLLOW ME ON TWITTER</a>
+		<a href= "..." ><img src="..." class="bottom__hire"> HIRE PARAVEL</a>
+		<a href="..."><img src=".." class="bottom__rss"> RSS FEED</a>
+
+	</div>
+
+
+</div>
+
+</body>
+
+</html>
---
+++ hulk.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="pear.css">
+	<style type="text/css">a {text-decoration: none}</style>
+</head>
+<body>
+
+<div class="header">
+
+
+	<img src="logo_TrentWalton.png" class="logo">
+	
+		<div class="page">
+			<a href="..." class="page__articles">ARTICLES</a>
+			<a href="..." class="page__notes">NOTES</a>
+			<a href="..." class="page__info">INFO</a>
+			<a href="..." class="page__search">SEARCH</a>
+		</div>
+
+		
+<div class= "main">
+
+	
+	<div class="articlehead">
+		<h3><a href= "..." class="article__title" >The Marvel Symphonic Universe</a></h3>
+		<a href="..." class="article__date">Sep 12th, 2016--</a>
+		<a href="..." class="article__responses">Leave a Responses</a>
+	</div>
+		<p class= "article__description">First off, every <a href="...">Every Frame a Painting</a> video is worth your time, but the latest one about the state of cinematic music compostion is extra fascinating.  I found lots of parallels for any industry that can be tempted to (over)imitate itself-especially when the process includes lots of people and phases.</p>
+
+	
+		
+			<p> Creativity and uniqueness take some risk.</p>
+
+			<video width="320" height="240" controls>
+  			<source src="movie.mp4" type="video/mp4">
+ 			Your browser does not support the video tag.
+			</video>
+		
+		
+
+
+			<p> Tagged:<a href="...">Design</a>,<a href="...">Process</a> </p>
+
+			<h3>0 Responses</h3>
+		
+				<a href="...">Leave a comment</a> or contact me via Twitter<a href="...">@TrentWalton</a>
+
+				<h3>Leave a Reply</h3>
+
+				<p><input type="text" name="wholename">Name (required)</p>
+				<p><input type="text" email="email">Email (required)</p>
+				<p><input type="text" website="website">Website</p>
+				<p><input></p>
+				<input type="submit">
+
+</div>
+
+<div class="footer"> 
+
+	<div class="bottom">
+		<a href= "..." > <img src="..." class="bottom__twitter">FOLLOW ME ON TWITTER</a>
+		<a href= "..." ><img src="..." class="bottom__hire"> HIRE PARAVEL</a>
+		<a href="..."><img src=".." class="bottom__rss"> RSS FEED</a>
+	</div>
+
+
+</div>
+
+
+
+</body>
+</html>
