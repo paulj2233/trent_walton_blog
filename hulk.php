1a6db53ec5657a429569b4471eed68d51cf66745
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-10-20 21:04:55 -0500

    builds out the single note view

--- hulk.php
+++ hulk.php
@@ -20,42 +20,45 @@
 		</div>
 </div>
 		
-<div class= "main">
+<div class= "notes-container">
 
 	
-	<div class="articlehead">
-		<a href= "..." class="articlehead__title" >The Marvel Symphonic Universe</a>
-	</div>
-	<div>
-		<a href="..." class="articlehead__dateresponse">Sep 12th, 2016-- Leave a Response</a>
-	</div>
-		<p class= "article__description">First off, every <a href="...">Every Frame a Painting</a> video is worth your time, but the latest one about the state of cinematic music compostion is extra fascinating.  I found lots of parallels for any industry that can be tempted to (over)imitate itself-especially when the process includes lots of people and phases.</p>
+	<a href="#" class="section__title" >The Marvel Symphonic Universe</a>
+	<a href="#" class="note__dateresponse">Sep 12th, 2016-- Leave a Response</a>
+
+	<p class="note__description">
+		First off, every <a href="#">Every Frame a Painting</a> video is worth your time, but the latest one about the state of cinematic music compostion is extra fascinating.  I found lots of parallels for any industry that can be tempted to (over)imitate itself-especially when the process includes lots of people and phases.
+	</p>
 
 	
 		
-			<p> Creativity and uniqueness take some risk.</p>
+	<p class="note__description">
+		Creativity and uniqueness take some risk.
+	</p>
 
-			<video width="320" height="240" controls>
-  			<source src="movie.mp4" type="video/mp4">
- 			Your browser does not support the video tag.
-			</video>
+	<div class="video-wrapper">
+		<iframe class="note__media" src="https://www.youtube.com/embed/7vfqkvwW2fs" frameborder="0" allowfullscreen></iframe>
+	</div> 
 		
+	<p class="note__description">Tagged: <a href="..."> Design</a>,<a href="..."> Process </a> </p>
+
+	<div class="responses">
+		<h3 class="h3 section__title">0 Responses</h3>
 		
+		<p class="comment"><a href="...">Leave a comment</a> or contact me via Twitter<a href="...">@TrentWalton</a></p>
+	</div>
 
+	<div class="form">
+		<h3 class="h3 section__title"">Leave a Reply</h3>
 
-			<p> Tagged: <a href="..."> Design</a>,<a href="..."> Process </a> </p>
-
-			<h3 class="h3">0 Responses</h3>
-		
-				<p class="comment"><a href="...">Leave a comment</a> or contact me via Twitter<a href="...">@TrentWalton</a></p>
-
-			<h3 class="h3">Leave a Reply</h3>
-
-				<p><input type="text" name="wholename" class= "input">Name (required)</p>
-				<p><input type="text" email="email" class= "input">Email (required)</p>
-				<p><input type="text" website="website" class= "input">Website</p>
-				<textarea name=""comment" class="textarea"></textarea>
-				<button type="submit" name="submit" class="button">Submit</button>
+		<form>
+			<p class="form-field"><input type="text" name="wholename" class="input">Name (required)</p>
+			<p class="form-field"><input type="text" email="email" class= "input">Email (required)</p>
+			<p class="form-field"><input type="text" website="website" class= "input">Website</p>
+			<textarea name=""comment" class="textarea"></textarea>
+			<button type="submit" name="submit" class="button">Submit</button>
+		</form>
+	</div>
 
 </div>
 
